f5c9df062820230daf48c2eb237f4cdc95eff1ed
Artemius <artemcka.jdanov@yandex.ru>
2025-02-24 23:47:18 +0700

    Create ToDoItem component

---
+++ vue-ts-app/src/components/ToDoItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <li></li>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+
+export default defineComponent({
+  props: {
+    todo: {
+      type: Object as PropType<Todo>,
+      required: true,
+    },
+  },
+  methods: {
+    deleteTodo() {
+      this.$emit('remove', this.todo);
+    },
+  },
+});
+</script>
+
+<style scoped>
+</style>
