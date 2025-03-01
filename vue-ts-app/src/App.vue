07c165e38ce13d49723010c99fad1a3f53f58029
Artemius <artemcka.jdanov@yandex.ru>
2025-03-01 23:22:07 +0700

    add TodoList in App.vue

--- vue-ts-app/src/App.vue
+++ vue-ts-app/src/App.vue
@@ -1,18 +1,13 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <h1>VueTs - TodoList</h1>
+    <ToDoList />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
+
+<script setup lang="ts">
+import ToDoList from './components/ToDoList.vue';
+</script>
 
 <style scoped>
 .logo {
